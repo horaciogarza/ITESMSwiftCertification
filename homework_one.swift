b0db284603dbe0a5573fa27950164cf1d0141a72
Horacio Garza <hgx95@hotmail.com>
2016-03-21 23:12:31 -0600

    Create homework_one.swift

---
+++ homework_one.swift
@@ -0,0 +1,23 @@
+//  TODO: Write some awesome Swift code, or import libraries like "Foundation",
+//  "Dispatch", or "Glibc"
+//Author: Horacio Garza
+
+//Profesor: esto fué hecho en https://swiftlang.ng.bluemix.net/ , debido a que aún no cuento con una Mac; en las siguientes tareas ya lo haré con una Mac, gracias.
+
+for i in 0...100{
+   
+    if i%5 == 0 {
+        print("\(i) Bingo!")
+    }
+    
+    if i%2 == 0 {
+        print("\(i) par!!!")
+    }else{
+        print("\(i) impar!!!")
+    }
+    
+    if i >= 30 && i <= 40{
+        print("\(i) Viva Swift!!!")
+    }
+    
+}
